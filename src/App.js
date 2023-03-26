24d3bdc8da5ce0d9bc8a0c5f3511bb271ca2674f
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2023-03-26 14:26:34 -0500

    small changes

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ function App() {
                 <Userlist/>
               </ProtectedRoute>} />
           <Route 
-            path="/doctor/profile/:userId"
+            path="/doctor/profile/:doctorId"
             element={
               <ProtectedRoute>
                 <Profile />
